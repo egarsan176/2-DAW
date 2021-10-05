8efaf4c589c4ee37b649e74a46c7bb1474a825a0
egarsan176 <estefaniagarci@gmail.com>
2021-10-05 14:00:11 +0200

    añadidos ejercicios objetos nativos completo

--- ENTORNO - CLIENTE/JAVASCRIPT/ACTIVIDADES OBJETOS NATIVOS/actividad4.js
+++ ENTORNO - CLIENTE/JAVASCRIPT/ACTIVIDADES OBJETOS NATIVOS/actividad4.js
@@ -2,3 +2,32 @@
 // https://developer.mozilla.org/es/docs/Web/API/Navigator indica y prueba algunos
 // métodos útiles del objeto navigator.
 
+let mostrar = document.getElementById("boton");
+
+mostrar.addEventListener("click", mostrarInfo);
+
+function mostrarInfo(){
+
+    let espacio = document.getElementById("datos");
+
+    let p1 = document.createElement("li");
+    p1.innerText = "Nombre del navegador: "+ navigator.appName;
+    espacio.appendChild(p1);
+
+    let p2 = document.createElement("li");
+    p2.innerText = "Versión del navegador: "+ navigator.appVersion;
+    espacio.appendChild(p2);
+
+    let p3 = document.createElement("li");
+    p3.innerText = "Idioma del navegador: "+ navigator.language;
+    espacio.appendChild(p3);
+
+    let p4 = document.createElement("li");
+    p4.innerText = "Idiomas conocidos por el usuario: "+ navigator.languages;
+    espacio.appendChild(p4);
+
+    let p5 = document.createElement("li");
+    p5.innerText = "Sistema operativo actual: "+ navigator.oscpu;
+    espacio.appendChild(p5);
+
+}
--- ENTORNO - CLIENTE/JAVASCRIPT/ACTIVIDADES OBJETOS NATIVOS/actividad5.js
+++ ENTORNO - CLIENTE/JAVASCRIPT/ACTIVIDADES OBJETOS NATIVOS/actividad5.js
@@ -1,3 +1,25 @@
 // Actividad 5: Revisa la documentación del objeto screen
 // https://developer.mozilla.org/es/docs/Web/API/Screen y
-// busca al menos 3 ejemplos útiles que puedas realizar con este objeto.
+// busca al menos 3 ejemplos útiles que puedas realizar con este objeto.let mostrar = document.getElementById("boton");
+
+let mostrar = document.getElementById("boton");
+
+mostrar.addEventListener("click", mostrarInfo);
+
+function mostrarInfo(){
+
+    let espacio = document.getElementById("datos");
+
+    let p1 = document.createElement("li");
+    p1.innerText = "Anchura de la pantalla: "+ screen.width;
+    espacio.appendChild(p1);
+
+    let p2 = document.createElement("li");
+    p2.innerText = "Altura de la pantalla: "+ screen.height;
+    espacio.appendChild(p2);
+
+    let p3 = document.createElement("li");
+    p3.innerText = "Profundidad de bits de la pantalla: "+ screen.pixelDepth;
+    espacio.appendChild(p3);
+
+}
